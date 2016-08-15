8cb798965f3d2b86443082d1a4b0faa5bf03bcaf
WangChun <wangchun@pindou.com>
2016-08-15 10:25:27 +0800

    update

--- src/parse/zycList.js
+++ src/parse/zycList.js
@@ -1,12 +1,11 @@
 var connect = require('../db/index');
 
 module.exports = function(callback) {
-	callback(null, []);
-   /* connect(function(err, db) {
+    connect(function(err, db) {
         if (err) callback(err, []);
         var collection = db.collection("zyc");
         collection.find().sort({'_id':-1}).limit(100).toArray(function(err, datas) {
             callback(err, datas);
         });
-    });*/
+    });
 }
