7200ae9e58750baa9f732e5c01b67b7a68889678
WangChun <wangchun@pindou.com>
2016-08-25 09:30:47 +0800

    rebase

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/test';
-
 module.exports = function(callback) {
     MongoClient.connect(url, function(err, db) {
        callback(err,db);
